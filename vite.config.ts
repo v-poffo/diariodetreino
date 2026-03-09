d548e56fec8da4fb0609050c0392e0b7aff968a3
v-poffo <vinipoffo@gmail.com>
2026-03-09 00:21:36 -0300

    Set base path for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/diariodetreino/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
